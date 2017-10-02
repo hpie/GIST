387329b8634b811e0d5532048d6b84f7b046416e
Shweta <shwetashanks@gmail.com>
2017-10-03 00:39:29 +0530

    API Key corrections

--- database/DB-Groups/001-02-cdac_gist_app_api_key.sql
+++ database/DB-Groups/001-02-cdac_gist_app_api_key.sql
@@ -1,17 +1,28 @@
 --
--- Drop table if exists `api_access`
+-- Drop table if exists `api_keys`
 --
 
-DROP TABLE IF EXISTS `api_access`;
+DROP TABLE IF EXISTS `api_keys`;
 
 --
--- Table structure for table `api_access`
+-- Table structure for table `api_keys`
 --
 
-CREATE TABLE IF NOT EXISTS `api_access` (
-  `id` int(11) unsigned NOT NULL,
-  `key` varchar(40) NOT NULL DEFAULT '',
-  `controller` varchar(50) NOT NULL DEFAULT '',
-  `date_created` datetime DEFAULT NULL,
-  `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `api_keys` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `key` varchar(40) NOT NULL,
+  `level` int(2) NOT NULL,
+  `ignore_limits` tinyint(1) NOT NULL DEFAULT '0',
+  `is_private_key` tinyint(1) NOT NULL DEFAULT '0',
+  `ip_addresses` text,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `api_keys`
+--
+
+INSERT INTO `api_keys` (`id`, `user_id`, `key`, `level`, `ignore_limits`, `is_private_key`, `ip_addresses`, `date_created`) VALUES
+(1, 0, 'anonymous', 1, 1, 0, NULL, 1463388382);
+
